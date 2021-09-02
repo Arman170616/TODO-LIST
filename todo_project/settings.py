4b188eb4e15fea8f7975898e4d04e1163628dd17
Arman170616 <armanice9@gmail.com>
2021-09-02 16:25:22 +0600

    serializer added

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -37,9 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Local
-    'todos.apps.TodosConfig'
+    # Local
+    'todos.apps.TodosConfig',
+
+    # 3rd party
+    'rest_framework',
+
 ]
+
+# new
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
